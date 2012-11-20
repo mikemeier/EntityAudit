d58bb5c2d9e33cdb6c777e2158690265fcb2aa39
Mike Meier <sk.mykon@gmail.com>
2012-11-20 14:00:43 +0100

    Update src/SimpleThings/EntityAudit/AuditReader.php
    Maybe correct fix for Issue #35

--- src/SimpleThings/EntityAudit/AuditReader.php
+++ src/SimpleThings/EntityAudit/AuditReader.php
@@ -127,7 +127,7 @@ class AuditReader
         $revisionData = array();
 
         foreach ($columnMap as $fieldName => $resultColumn) {
-            $revisionData[$fieldName] = $row[$resultColumn];
+            $revisionData[$fieldName] = $row[$fieldName];
         }
 
         return $this->createEntity($class->name, $revisionData);
